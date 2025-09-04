70adc25e7b352f0991bc509aa2de0fb6fdaec7f4
Bltkk <baltazarmoran@MacBook-Air-de-Baltazar.local>
2025-09-03 21:28:43 -0400

    Actualiza estilos y agrega información del proyecto

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
-document.getElementById('showRegister').onclick = function() {
-    document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('registerForm').style.display = 'block';
+function $(id) {
+    return document.getElementById(id);
+}
+
+$('showRegister').onclick = function() {
+    $('loginForm').style.display = 'none';
+    $('registerForm').style.display = 'block';
 };
-document.getElementById('cancelRegister').onclick = function() {
-    document.getElementById('registerForm').style.display = 'none';
-    document.getElementById('loginForm').style.display = 'block';
+$('cancelRegister').onclick = function() {
+    $('registerForm').style.display = 'none';
+    $('loginForm').style.display = 'block';
 };
-document.getElementById('loginForm').addEventListener('submit', function(e) {
+$('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('navLinks').style.display = 'block';
+    $('loginForm').style.display = 'none';
+    $('navLinks').style.display = 'block';
 });
-document.getElementById('registerForm').addEventListener('submit', function(e) {
+$('registerForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    document.getElementById('registerForm').style.display = 'none';
-    document.getElementById('navLinks').style.display = 'block';
+    $('registerForm').style.display = 'none';
+    $('navLinks').style.display = 'block';
 });
